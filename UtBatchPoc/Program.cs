aef74529f14e22a16f545fdf286171e497a86b7f
Jacob Boswell <jacobboswell@utah.gov>
2018-11-06 14:40:11 -0700

    added readme

---
+++ UtBatchPoc/Program.cs
@@ -0,0 +1,17 @@
+﻿using ArBatch;
+using Summer.Batch.Core;
+
+namespace UtBatchPoc
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configurator = new ArBatchJobConfig();
+            var job = configurator.GetXmlJob();
+            var jobExecution = JobStarter.Start(job, new ArBatchUnityLoader());
+            /*
+            */
+        }
+    }
+}
